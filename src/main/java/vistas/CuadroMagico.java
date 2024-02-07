bf1b27f867f65de3456ebdb1a4dd57ac28c7437a
Saga <emmanuel.blancox5@gmail.com>
2024-02-06 18:26:35 -0600

    Screen Cuadro Magico

---
+++ src/main/java/vistas/CuadroMagico.java
@@ -0,0 +1,17 @@
+package vistas;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class CuadroMagico extends Stage {
+    private Scene escena;
+    public CuadroMagico(){
+        this.setTitle("Cuadro Magico");
+        this.setScene(new Scene(new Button("Da click")));
+        this.show();
+    }
+    private void CrearUI(){
+        escena = new Scene(new Button("Da click"));
+    }
+}
