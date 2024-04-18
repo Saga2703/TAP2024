50ec5256e8d36f780c71288a4685be1df05b6e96
angel <mylastshotwashere14106@protonmail.com>
2024-04-17 22:55:51 -0600

    Added details

--- src/main/java/com/example/tap2024/vistas/TaqueriaGUI.java
+++ src/main/java/com/example/tap2024/vistas/TaqueriaGUI.java
@@ -1,10 +1,31 @@
 package com.example.tap2024.vistas;
 
+import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TaqueriaGUI extends Stage {
 
+    private Scene escena;
     private MenuBar mnbPrincipal;
+    private GridPane mesas;
+    private HBox mnTiposAlimentos;
+    private GridPane mnAlimentos;
+    private VBox mnOrden;
+    private HBox mnPrincipal;
+
+    public TaqueriaGUI(){
+        CrearUI();
+        this.setTitle("Taqueria");
+        this.setScene(escena);
+        this.show();
+    }
+
+    public void CrearUI(){
+
+    }
 
 }
