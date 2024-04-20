704f5a71a11f445c2e21ba71e6361ab5882d1404
angel <mylastshotwashere14106@protonmail.com>
2024-04-20 12:31:01 -0600

    Modifique AdminTaqueriasGUI

--- src/main/java/com/example/tap2024/vistas/AdminTaqueriaGUI.java
+++ src/main/java/com/example/tap2024/vistas/AdminTaqueriaGUI.java
@@ -1,6 +1,32 @@
 package com.example.tap2024.vistas;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
 
 public class AdminTaqueriaGUI extends Stage {
+
+
+    private Scene escena;
+    private HBox userMN;
+    private HBox pwdMN;
+    private Button validar;
+    private boolean validado;
+
+    AdminTaqueriaGUI(){
+        CrearUI();
+        this.setTitle("Administracion");
+        this.setScene(escena);
+        this.show();
+    }
+
+    public void CrearUI(){
+
+    }
+
+    public void CrearMenuModificaciones(){
+
+    }
+
 }
