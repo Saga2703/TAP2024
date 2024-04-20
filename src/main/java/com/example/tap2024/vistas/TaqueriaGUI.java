e7b8616fc35f2dc93bd1ee4d56b80ec9403b8da5
angel <mylastshotwashere14106@protonmail.com>
2024-04-20 11:57:07 -0600

    Agregue CrearMNAlimentos

--- src/main/java/com/example/tap2024/vistas/TaqueriaGUI.java
+++ src/main/java/com/example/tap2024/vistas/TaqueriaGUI.java
@@ -3,6 +3,7 @@ package com.example.tap2024.vistas;
 import com.example.tap2024.modelos.*;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
 import javafx.scene.image.Image;
@@ -47,7 +48,6 @@ public class TaqueriaGUI extends Stage {
 
     public void CrearMNOrden(){
         CrearMNTiposAlimentos();
-        CrearMNAlimentos();
         CrearBAlimentos();
         mnOrden = new VBox(mnTiposAlimentos,mnAlimentos,bAlimentos);
         mnPrincipal.getChildren().add(mnOrden);
@@ -56,24 +56,35 @@ public class TaqueriaGUI extends Stage {
     public void CrearMNTiposAlimentos(){
         Image imgTipo;
         ImageView imgTipoView;
-        Label lbTipo;
+        Button bTipo;
         VBox temp = new VBox();
         mnTiposAlimentos = new HBox();
         ObservableList<CategoriaDAO> tipos = categoria.CONSULTAR();
         for(int tipo = 0; tipo < tipos.size(); tipo++){
             imgTipo = ObtenerImg.obtImg(tipos.get(tipo).getCategoria());
             imgTipoView = new ImageView(imgTipo);
-            lbTipo = new Label(tipos.get(tipo).getCategoria());
-            temp = new VBox(imgTipoView,lbTipo);
+            bTipo = new Button(tipos.get(tipo).getCategoria());
+            int tempS = tipos.get(tipo).getId_categoria();
+            bTipo.setOnAction(event -> CrearMNAlimentos(tempS));
+            temp = new VBox(imgTipoView,bTipo);
             mnTiposAlimentos.getChildren().add(temp);
         }
     }
 
-    public void CrearMNAlimentos(){
+    public void CrearMNAlimentos(int categoria){
         Image imgAlim;
         ImageView imgAlimView;
-        Label lbAlim;
+        Button bAlim;
+        VBox temp = new VBox();
+        ObservableList<ProductoDAO> alimentos = productos.CONSULTAR();
         mnAlimentos = new GridPane();
+        for(int alimento = 0; alimento < alimentos.size(); alimento++){
+            imgAlim = ObtenerImg.obtImg(alimentos.get(alimento).getProducto());
+            imgAlimView = new ImageView(imgAlim);
+            bAlim = new Button(alimentos.get(alimento).getProducto());
+            temp = new VBox(imgAlimView, bAlim);
+            mnAlimentos.add(temp,alimento%5,alimento/5);
+        }
     }
 
     public void CrearBAlimentos(){
