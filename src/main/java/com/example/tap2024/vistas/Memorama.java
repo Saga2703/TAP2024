2c12e8c1cd1d55bf4ede1438fd0433414a95b749
Saga <emmanuel.blancox5@gmail.com>
2024-06-06 13:13:20 -0600

    Memorama2

--- src/main/java/com/example/tap2024/vistas/Memorama.java
+++ src/main/java/com/example/tap2024/vistas/Memorama.java
@@ -1,5 +1,6 @@
 package com.example.tap2024.vistas;
 
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -26,8 +27,14 @@ public class Memorama extends Stage {
     private GridPane gdpImagenes;
     private Label lblPares, lbl1,lbl2,lbl3,lbl4,lblTimer;
     private BorderPane bdpAll;
-    private int numPares = 3; // Número de pares de imágenes
     private ArrayList<String> listaImagenes;
+    private String reversoPath = "/imagenes/reverso.jpg";
+    private int turnoActual = 1;
+    private int paresJugador1 = 0;
+    private int paresJugador2 = 0;
+    private Button primeraCarta;
+    private Button segundaCarta;
+    private boolean esperandoVolteo = false;
 
 
     public Memorama(){
@@ -53,10 +60,14 @@ public class Memorama extends Stage {
         lbl4 = new Label("...");
 
         hContenedor3 = new HBox(lbl1, lbl3);
+        hContenedor3.setStyle("-fx-background-color: white;");
+
         hContenedor3.setSpacing(20);
         hContenedor3.setPadding(new Insets(10));
 
         hContenedor4 = new HBox(lbl2, lbl4);
+        hContenedor4.setStyle("-fx-background-color: white;");
+
         hContenedor4.setSpacing(20);
         hContenedor4.setPadding(new Insets(10));
 
@@ -74,10 +85,12 @@ public class Memorama extends Stage {
         bdpAll = new BorderPane(vContenedor1);
         escena = new Scene(bdpAll, 800, 600);
 
-        RevolverCartas(0); // Inicialmente revuelve las cartas con 8 pares
+
     }
 
     private void actualizarNumParesYRevolver() {
+        hContenedor3.setStyle("-fx-background-color: white;");
+        hContenedor4.setStyle("-fx-background-color: white;");
         try {
             int nuevoNumPares = Integer.parseInt(txtPantalla.getText());
             if (nuevoNumPares < 3 || nuevoNumPares > 15) {
@@ -98,6 +111,7 @@ public class Memorama extends Stage {
     }
     private void RevolverCartas(int numPares) {
         gdpImagenes.getChildren().clear();
+        resetGame();
 
         String[] arImagenes = {
                 "link oot.jpg", "sheik.jpg", "zelda.jpg", "trifuerza.jpg",
@@ -115,33 +129,119 @@ public class Memorama extends Stage {
 
         Button[][] arBtncartas = new Button[4][numPares];
 
+        Image reversoImage = new Image(getClass().getResource(reversoPath).toString());
         int pos = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < numPares; j++) {
                 if (pos < listaImagenes.size()) {
                     arBtncartas[i][j] = new Button();
-
-                    // Load image from listaImagenes
-                    String imageName = listaImagenes.get(pos);
-                    Image image = null;
-                    try {
-                        image = new Image(getClass().getResource("/imagenes/" + imageName).toString());
-                    } catch (Exception e) {
-                        mostrarError("No se pudo cargar la imagen: " + imageName);
-                    }
-
-                    if (image != null) {
-                        ImageView imageView = new ImageView(image);
-                        imageView.setFitHeight(150);
-                        imageView.setFitWidth(100);
-
-                        arBtncartas[i][j].setGraphic(imageView);
-                    }
+                    ImageView reversoView = new ImageView(reversoImage);
+                    reversoView.setFitHeight(150);
+                    reversoView.setFitWidth(100);
+                    arBtncartas[i][j].setGraphic(reversoView);
                     arBtncartas[i][j].setPrefSize(100, 150);
+
+                    String imageName = listaImagenes.get(pos);
+                    ImageView frenteView = new ImageView(new Image(getClass().getResource("/imagenes/" + imageName).toString()));
+                    frenteView.setFitHeight(150);
+                    frenteView.setFitWidth(100);
+
+                    presionarCarta(arBtncartas[i][j], reversoView, frenteView);
+
                     gdpImagenes.add(arBtncartas[i][j], j, i);
                     pos++;
                 }
             }
         }
     }
+
+    private void presionarCarta(Button btnCarta, ImageView reversoView, ImageView frenteView) {
+        btnCarta.setOnAction(e -> {
+            if (!esperandoVolteo) {
+                // Verificar si la carta ya está volteada
+                if (btnCarta.getGraphic() == frenteView) {
+                    return; // Salir del método si la carta ya está volteada
+                }
+
+                // Voltear la carta
+                btnCarta.setGraphic(frenteView);
+
+                if (primeraCarta == null) {
+                    primeraCarta = btnCarta;
+                } else if (segundaCarta == null) {
+                    segundaCarta = btnCarta;
+                    verificarPareja();
+                }
+            }
+        });
+    }
+
+    private void verificarPareja() {
+        esperandoVolteo = true;
+
+        // Obtener las imágenes de las cartas
+        ImageView imagenPrimeraCarta = (ImageView) primeraCarta.getGraphic();
+        ImageView imagenSegundaCarta = (ImageView) segundaCarta.getGraphic();
+
+        if (imagenPrimeraCarta.getImage().getUrl().equals(imagenSegundaCarta.getImage().getUrl())) {
+            // Las imágenes son iguales
+            Platform.runLater(() -> {
+                if (turnoActual == 1) {
+                    paresJugador1++;
+                    lbl3.setText(String.valueOf(paresJugador1));
+                } else {
+                    paresJugador2++;
+                    lbl4.setText(String.valueOf(paresJugador2));
+                }
+                resetTurno();
+                esperandoVolteo = false;
+            });
+        } else {
+            // Las imágenes no son iguales
+            new Thread(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                // Voltear las cartas de nuevo
+                Platform.runLater(() -> {
+                    primeraCarta.setGraphic(new ImageView(getClass().getResource(reversoPath).toString()));
+                    segundaCarta.setGraphic(new ImageView(getClass().getResource(reversoPath).toString()));
+                    cambiarTurno();
+                    esperandoVolteo = false;
+                });
+            }).start();
+        }
+    }
+
+
+    private void cambiarTurno() {
+        if (turnoActual == 1) {
+            turnoActual = 2;
+            hContenedor3.setStyle("-fx-background-color: red;");
+            hContenedor4.setStyle("-fx-background-color: green;");
+        } else {
+            turnoActual = 1;
+            hContenedor3.setStyle("-fx-background-color: green;");
+            hContenedor4.setStyle("-fx-background-color: red;");
+        }
+        resetTurno();
+    }
+
+    private void resetTurno() {
+        primeraCarta = null;
+        segundaCarta = null;
+    }
+
+    private void resetGame() {
+        turnoActual = 1;
+        paresJugador1 = 0;
+        paresJugador2 = 0;
+        lbl3.setText("0");
+        lbl4.setText("0");
+
+        resetTurno();
+    }
+
 }
